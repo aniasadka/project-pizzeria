3667ec6416abedb5984615c86cd422abfd7277a3
Anna Sadka <anna.sadka87@gmail.com>
2020-08-14 18:49:48 +0200

    task 8.6 - images

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
-
 {
   'use strict';
 
@@ -93,6 +92,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -113,42 +113,78 @@
         thisProduct.element.classList.toggle('active');
       });
     }
-  }
 
-  initOrderForm() {
-    const thisProduct = this;
-    console.log('initOrderForm');
+    initOrderForm() {
+      const thisProduct = this;
+      console.log('initOrderForm');
 
-    thisProduct.form.addEventListener('submit', function (event) {
-      event.preventDefault();
-      thisProduct.processOrder();
-    });
-
-    for (let input of thisProduct.formInputs) {
-      input.addEventListener('change', function () {
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
         thisProduct.processOrder();
       });
 
-      thisProduct.cartButton.addEventListener('click', function (event) {
-        event.preventDefault();
-        thisProduct.processOrder(;)
-      });
-    }
-  }
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder();
+        });
 
-  processOrder() {
-    const thisProduct = this;
-    console.log('processOrder');
-
-    const formData = utils.serializeFormToObject(thisProduct.form);
-    thisProduct = params {}
-    let price = thisProduct.data.price;
-
-    for (let paramId in this.Product.data.params) {
-      const param = thisProduct.data.params[paramId];
+        thisProduct.cartButton.addEventListener('click', function (event) {
+          event.preventDefault();
+          thisProduct.processOrder();
+        });
+      }
     }
 
-    console.log('formData', formData);
+    processOrder() {
+      const thisProduct = this;
+      console.log('processOrder');
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      thisProduct.params = {};
+
+      let price = thisProduct.data.price;
+
+      for (let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+
+        for (let optionId in param.options) {
+          const option = param.options[optionId];
+
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+
+          if (optionSelected && !option.default) {
+            price += option.price;
+          } else if (!optionSelected && option.default) {
+            price -= option.price;
+          }
+
+          const imageClass = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
+
+          if (optionSelected) {
+            if (!thisProduct.params[paramId]) {
+              thisProduct.params[paramId] = {
+                label: param.label,
+                options: {},
+              };
+            }
+            thisProduct.params[paramId].options[optionId] = option.label;
+
+            for (let singleClass of imageClass) {
+              singleClass.classList.add(classNames.menuProduct.imageVisible);
+            }
+          } else {
+            for (let singleClass of imageClass) {
+              singleClass.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
+        }
+      }
+
+
+
+      //thisProduct.priceElem.innerHTML = price;
+    }
   }
 
 
